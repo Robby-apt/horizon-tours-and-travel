c0ef396ceca1673f48b93e7d5b46415ac42e2ec6
Robby-apt <robin.kw99@yahoo.com>
2024-04-02 13:38:34 +0300

    Setup of backend

--- frontend/src/components/bookings-components/BookingForm.jsx
+++ frontend/src/components/bookings-components/BookingForm.jsx
@@ -27,7 +27,7 @@ function BookingForm() {
 			let dataBody = JSON.stringify(bookingInfo);
 
 			try {
-				const url = `http://localhost:3001`;
+				const url = process.env.REACT_APP_BACKEND_PORT;
 				let res = await fetch(url, {
 					method: 'POST',
 					headers: { 'content-type': 'application/json' },
